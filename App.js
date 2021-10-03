6c7bc8600bb44a0f3029645a264a500ff7c6f033
jaemin <woals8115@naver.com>
2021-10-03 23:20:39 +0900

    v3

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { react } from '@babel/types';
 import React, {Component} from 'react';
 import {
+  Alert,
   AppState,
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -13,8 +15,21 @@ import {
 import Custom_bt from './Custom_bt';
 
 
-
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state={count:0};
+    
+  }
+  _updateCount(){
+    this.setState({
+      count:this.state.count+1,
+    });
+    // this.setState((prevState, props) => {
+    //   return {count:prevState.count+1}
+    // });
+  }
 
   render() {
     return(
@@ -23,13 +38,24 @@ export default class App extends React.Component {
         <View style={styles.content}> 
             <View style={styles.profile}/>
             <Text> 안녕하세요 </Text>
-              <Custom_bt>{this.props.name}</Custom_bt>
+            <Custom_bt
+            id={'login'}
+            title={'회원가입'}
+            onPress= {()=>alert('hs')}
+            ></Custom_bt>
+
+            <Button
+            color="green"
+            title={this.state.count.toString()}
+            onPress={this._updateCount.bind(this)}></Button>
+          
         </View>
         <View style={styles.footer} />
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container:{
     flex:1,
--- Custom_bt.js
+++ Custom_bt.js
@@ -3,16 +3,21 @@ import{
     TouchableOpacity,
     Text,
     StyleSheet,
+    Button,
 }from 'react-native';
 
 export default class Custom_bt extends Component{
 
     render(){
         return(
-            <Custom_bt name="리액트" />,
-            <TouchableOpacity>
-                <Text>회원가입</Text>
-            </TouchableOpacity>
+         
+                <TouchableOpacity
+                onPress={this.props.onPress}>
+                 <Text>회원가입</Text>
+                </TouchableOpacity>
+                
+            
         )
     }
 }
+
